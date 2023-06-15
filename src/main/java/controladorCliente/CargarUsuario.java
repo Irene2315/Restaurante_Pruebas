cd98d980132e408e77d992af838a988524a0a195
Irene <ireneherrero@outlook.com>
2023-06-15 12:13:43 +0200

    traspaso datos

--- src/main/java/controladorCliente/CargarUsuario.java
+++ src/main/java/controladorCliente/CargarUsuario.java
@@ -59,7 +59,7 @@ public class CargarUsuario extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		doGet(request, response);
 	}
 
